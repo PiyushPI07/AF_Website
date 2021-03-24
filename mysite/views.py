8c16b393485c582727ad661ba2287b9eea22afcb
Piyush Ingale <45006839+PiyushPI07@users.noreply.github.com>
2021-03-24 18:48:30 +0530

    Bug fixes (#34)
    * Added udaan carousal and events
    
    * removed background image field from udaan_static module and moved it to assets
    
    * removed redundant assets
    
    * added gitignore
    
    * added gitignore
    
    * Schema and Views Refactor
    
    * Fixes
    
    * added new account app around which recruitments model is built, frontend template reaggangements and dynamic routing in navbar
    
    * bug fix in RecruitmentApplicant cascaded deletion
    
    * added sand art competition form, volunteer form
    
    * UI/UX and Views.py Fixes
    
    * UI/UX and Views.py Fixes
    
    * bug fix in mysite.views
    
    * added new frontend
    
    * added testimonial section, alumni tab, code refractoring
    
    * deleted account apps backup views
    
    * deleted new_frontend branch
    
    * pulled changes from af remote
    
    * fixed teams page bugs
    
    * bug fixes udaan page
    
    * bug fixes: pre-loader, Artists forum typo, contact us div link
    
    * changed text justification for about us paragraph
    
    * merged migrations
    
    * deleted pycache files
    
    * deleted pycache files
    
    * added landing animation and AF font
    
    * pre release bug fixes
    
    * context-processor bug
    
    * added executive teams and bug fixes
    
    * changed admin view for member model

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseNotFound
 from account.forms import VolunteerForm, SandArtRegistrationForm
 from django.views.decorators.cache import cache_control
 from django.shortcuts import get_object_or_404, render
@@ -52,18 +52,6 @@ def blog_post(request,pk):
         'blog': blog,
         'comments': comments
     }
-    # if request.POST:
-    #     form = CommentForm(request.POST)
-    #     if form.is_valid():
-    #         form_data = form.save(commit=False)
-    #         form_data.post = blog
-    #         form_data.save()
-    #         HttpResponseRedirect('')
-    #     else:
-    #         context['form'] = form
-    # else:
-    #     form = CommentForm()
-    #     context['form'] = form
     if request.POST:
         author = request.POST.get("txtname")
         email = request.POST.get("txtemail")
@@ -74,25 +62,10 @@ def blog_post(request,pk):
     return render(request, 'blog_post.html', context=context)
 
 
-# def udaan(request):
-#     full_member_list = Member.objects.all()
-#     image_gallery_full = Image.objects.all()
-#     art_list = Art.objects.all()
-#     blog_list = Blog.objects.all()
-#     event_list = Event.objects.all()
-#     image_gallery_short = Image.objects.filter(display_on_index=True)
-#     context = {
-#         'full_member_list': full_member_list,
-#         'image_gallery_full': image_gallery_full,
-#         'art_list': art_list,
-#         'blog_list': blog_list,
-#         'event_list': event_list,
-#         'image_gallery_short': image_gallery_short
-#     }
-#     return render(request, 'udaan.html', context=context)
-
 def udaan_view(request, *args, **kwargs):
-    static_data= StaticContent.objects.get(id=1)
+    if StaticContent.objects.count() == 0:
+        return HttpResponseNotFound("<h1>Page not found</h1>")
+    static_data= StaticContent.objects.first()
     carousel_imgs = Udaan_image.objects.filter(display_on_caurosel = True)
     events = Udaan_event.objects.all()
     context = {
@@ -116,13 +89,22 @@ def udaan_view(request, *args, **kwargs):
     return render(request, 'udaan.html', context)
 
 def check_back_later(request, t):
-    dates = StaticContent.objects.get(id=1)
-    if t == 's_art404':
-        disp_text = 'Sand Art Competition '
-        date = dates.udaan_date.strftime('%B')
-    if t == 'recs404':
-        disp_text = 'Recruitment'
-        date = dates.recs_date.strftime('%B')
+    if StaticContent.objects.count():
+        dates = StaticContent.objects.first()
+        if t == 's_art404':
+            disp_text = 'Sand Art Competition '
+            date = dates.udaan_date.strftime('%B')
+        if t == 'recs404':
+            disp_text = 'Recruitment'
+            date = dates.recs_date.strftime('%B')
+    else:
+        if t == 's_art404':
+            disp_text = 'Sand Art Competition '
+            date = "January"
+        if t == 'recs404':
+            disp_text = 'Recruitment'
+            date = "August"
+        
     context = {
         'disp_text': disp_text,
         'date': date
@@ -164,11 +146,19 @@ def contact(request):
 def team(request):
     members = Member.objects.filter(active=True).order_by('post', 'batch', 'member_name')
     year = datetime.datetime.now().year
-    batches_to_display = [year-1, year-2, year-3, year-4]
+    batches_to_display = [year-1, year-2, year-3]
+    exc_members = members.filter(team="")
+    core_members = members.filter(team="Core")
+    mnc_members = members.filter(team__in=["Media and Content Team", "Both Media and Website Team"])
+    web_members = members.filter(team__in=["Website Team", "Both Media and Website Team"])
     alumni = Member.objects.filter(batch__in=batches_to_display).order_by('post', 'member_name')
     context = {
         'members': members,
         'batches_to_display': batches_to_display,
-        'alumni': alumni
+        'alumni': alumni,
+        'executive_members': exc_members,
+        'core_members' : core_members,
+        'med_and_content_members': mnc_members,
+        'web_members' : web_members
     }
     return render(request, 'team.html', context=context)
